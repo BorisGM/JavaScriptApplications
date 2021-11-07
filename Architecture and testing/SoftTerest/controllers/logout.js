b11815574c7d3645eb7f0cea418353857c07274f
BorisGM <93388190+BorisGM@users.noreply.github.com>
2021-11-07 13:55:12 +0200

    Delete Architecture and testing/SoftTerest directory

--- Architecture and testing/SoftTerest/controllers/logout.js
+++
@@ -1,8 +0,0 @@
-import { logoutReq } from '../data/api.js';
-import { getUserData, removeUserData } from '../data/userData.js';
-
-export const logout = () => {
-    const userData = getUserData();
-    logoutReq(userData.token);
-    removeUserData();
-};
