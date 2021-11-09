2ae6a4b18b585b48ce13ff94920bd336e3d30e6f
BorisGM <93388190+BorisGM@users.noreply.github.com>
2021-11-09 22:04:56 +0200

    Delete HTTP-status-cats directory

--- HTTP-status-cats/app.js
+++
@@ -1,8 +0,0 @@
-
-import { render } from './node_modules/lit-html/lit-html.js';
-import { catsView } from './catsView.js';
-import { cats } from './catSeeder.js';
-
-const root = document.getElementById('allCats');
-
-render(catsView(cats), root);
